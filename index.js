b1b3495baa818b1a9048f30284964f776005a988
agkayster <ejike.chiboka@gmail.com>
2023-08-18 11:45:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+// const port = 3000;
+
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+	res.send('Hello World!');
+});
+
+// app.listen(port, () => {
+// 	console.log(`Server is up and running on port ${port}`);
+// });
+
+module.exports.handler = serverless(app);
